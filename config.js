7605bdc490faeea180c8ba81624bf26c358181d6
Bob Barber <byetimmy@gmail.com>
2016-07-17 17:38:39 -0400

    Update config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
     'trains': [{
-        'trainno': 2252, //66,
+        'trainno': 66,
         'threshold': 20, //number of minutes late to trigger an alert
         'station': 'PVD',
-        'sendto': ['byetimmy@gmail.com', 'bob_barber@yahoo.com'],
-        'schedule': '*/20 * * * *' /* every 2 minutes */
+        'sendto': ['email1@address.com', 'email2@address.com'],
+        'schedule': '*/20 * * * *' /* every 20 minutes */
     }],
 
     dataUrl: 'http://dixielandsoftware.net/Amtrak/solari/data/{station}_schedule.php',
